d240f40ab40d118a09b34cd10e2d34ab1bd316cf
Joost Meijles <joost@meijles.com>
2019-10-31 14:14:27 +0100

    Add service worker

---
+++ static/service-worker.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const CACHE_NAME = 'v1.0.0'; //generate uniqueid
+
+const FILES_TO_CACHE = [
+    '/index.html',
+    'jss_product_cluster/index.html'
+];
+
+self.addEventListener('install', (evt) => {
+    console.log('[ServiceWorker] Install');
+    
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+          console.log('[ServiceWorker] Pre-caching offline page');
+          return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+    console.log('[ServiceWorker] Activate');
+    
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+              console.log('[ServiceWorker] Removing old cache', key);
+              return caches.delete(key);
+            }
+          }));
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    
+    if (evt.request.mode !== 'navigate') {
+        // Not a page navigation, bail.
+        return;
+    }
+
+    evt.respondWith(
+        fetch(evt.request)
+            .catch(() => {
+            return caches.open(CACHE_NAME)
+                .then((cache) => {
+                    // Fallback to cached index
+                    return cache.match('index.html');
+                });
+            })
+    );
+});
