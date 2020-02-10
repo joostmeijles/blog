13e3cc0c4cf42edc27a7b4c7944cfa4882ff881e
Joost <joost@meijles.com>
2020-02-10 21:15:38 +0100

    Fix tags

--- pages/tags/[tag].js
+++ pages/tags/[tag].js
@@ -14,12 +14,5 @@ export async function unstable_getStaticProps({ params }) {
 }
 
 export async function unstable_getStaticPaths() {
-  const tags = getAllTags()
-
-  const paths = []
-  for (let i = 0; i < tags; i++) {
-    paths.push({ params: { n: `${i}` } })
-  }
-
-  return paths
+  return getAllTags().map(tag => { return { params: { tag } } })
 }
