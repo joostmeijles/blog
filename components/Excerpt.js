06db6dd1b8b29f36e7b07a5057ce891144788e0b
Joost <joost@meijles.com>
2020-02-11 22:26:06 +0100

    Format excerpt body

--- components/Excerpt.js
+++ components/Excerpt.js
@@ -1,3 +1,4 @@
+import Markdown from 'react-markdown'
 import Link from 'next/link'
 import ArticleHeader from './ArticleHeader'
 
@@ -7,7 +8,7 @@ export default ({ md }) => {
   return (<div className="flex flex-col mb-5 mt-5">
     <ArticleHeader date={date} tags={tags} title={title} slug={slug} />
     <Link href="/[slug]" as={`/${slug}`}>
-      <p className="font-sans font-light cursor-pointer">{md.excerpt}</p>
+      <Markdown className="article cursor-pointer" source={md.excerpt}/>
     </Link>
     <Link href="/[slug]" as={`/${slug}`}>
       <a className="font-sans font-light underline p-2 rounded-lg hover:bg-gray-200 self-end">Read more</a>
