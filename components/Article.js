72532ee09264324f6d16d2218301746b0b9fe8ac
Joost <joost@meijles.com>
2020-02-11 22:15:30 +0100

    Add article styling

--- components/Article.js
+++ components/Article.js
@@ -5,7 +5,7 @@ export default ({ date, title, tags, canonical, content }) => {
   return (<>
     {/* canonical */}
     <ArticleHeader date={date} title={title} tags={tags} />
-    <Markdown source={content} />
+    <Markdown source={content} className="article" />
     {/* <SimilarArticles/> */}
   </>)
 }
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const Older = ({ nextPage }) => (
   </Link>
 )
 
-export default ({ articles, nextPage }) => ( 
+export default ({ articles, nextPage }) => (
   <Layout>
     {articles.map((md, i) => <Excerpt key={i} md={md}/>)}
     {nextPage !== undefined && <Older nextPage={nextPage}/>}
