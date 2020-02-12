dc0e954d2768a69dbed85af87c441a5d87537458
Joost <joost@meijles.com>
2020-02-12 21:14:40 +0100

    Add comments

--- components/Article.js
+++ components/Article.js
@@ -1,4 +1,5 @@
 import Markdown from 'react-markdown'
+import ReactUtterences from 'react-utterances'
 import ArticleHeader from './ArticleHeader'
 
 export default ({ date, title, tags, canonical, content }) => {
@@ -7,5 +8,6 @@ export default ({ date, title, tags, canonical, content }) => {
     <ArticleHeader date={date} title={title} tags={tags} />
     <Markdown source={content} className="article" />
     {/* <SimilarArticles/> */}
+    <ReactUtterences repo='joostmeijles/blog' type='url'/>
   </>)
 }
