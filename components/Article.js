d81ee0fbecf714a8c5e186e1042aa9733e77e9b9
Joost <joost@meijles.com>
2020-02-12 21:32:00 +0100

    Add utterances native

--- components/Article.js
+++ components/Article.js
@@ -1,5 +1,5 @@
+import React, { useEffect } from 'react'
 import Markdown from 'react-markdown'
-import ReactUtterences from 'react-utterances'
 import ArticleHeader from './ArticleHeader'
 
 export default ({ date, title, tags, canonical, content }) => {
@@ -8,6 +8,28 @@ export default ({ date, title, tags, canonical, content }) => {
     <ArticleHeader date={date} title={title} tags={tags} />
     <Markdown source={content} className="article" />
     {/* <SimilarArticles/> */}
-    <ReactUtterences repo='joostmeijles/blog' type='url'/>
+    <Utterances/>
   </>)
+}
+
+const Utterances = () => {
+  useEffect(() => {
+    const script = document.createElement('script')
+
+    script.src = 'https://utteranc.es/client.js'
+    script.setAttribute('repo', 'joostmeijles/blog')
+    script.setAttribute('issue-term', 'url')
+    script.label = 'comment'
+    script.setAttribute('theme', 'github-light')
+    script.setAttribute('crossorigin', 'anonymous')
+    script.async = true
+
+    document.body.appendChild(script)
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
+
+  return <div></div>
 }
