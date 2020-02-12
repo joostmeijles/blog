3a3915184216bf62276bae83912433513f9b4d42
Joost <joost@meijles.com>
2020-02-12 21:56:08 +0100

    Remove momentjs

--- components/ArticleHeader.js
+++ components/ArticleHeader.js
@@ -1,16 +1,33 @@
-import moment from 'moment'
 import Link from 'next/link'
 import Tags from './Tags'
 
-export default ({ tags, date, title, slug }) => (
-  <div className="flex flex-col">
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
+
+export default ({ tags, date, title, slug }) => {
+  const d = new Date(date)
+  const dateStr = `${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`
+
+  return (<div className="flex flex-col">
     <div className="flex flex-row justify-between">
-      <h2 className="font-sans text-sm text-gray-600 font-hairline">{moment(date).format('MMMM DD, YYYY')}</h2>
+      <h2 className="font-sans text-sm text-gray-600 font-hairline">{dateStr}</h2>
       <Tags tags={tags} />
     </div>
     <Title slug={slug} title={title} />
-  </div>
-)
+  </div>)
+}
 
 const Title = ({ slug, title }) => {
   const h = <h1 className="font-sans text-2xl font-thin hover:underline">{title}</h1>
