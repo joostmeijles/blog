c7b9cfaf04f6d5fa24b3263f9eb761d246a64eb2
Joost <joost@meijles.com>
2020-02-14 20:12:10 +0100

    Use kofi.png

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default () => (
   <div className="flex flex-col items-center mb-8 mt-8">
     <a href="https://ko-fi.com/G2G5114YM" target="_blank" rel="noopener noreferrer" className={styles.hvr_buzz_out}>
       <img className="w-32"
-        src={require('../public/kofi.webp')} border="0"
+        src={require('../public/kofi.png')} border="0"
         alt="Buy Me a Coffee at ko-fi.com" />
     </a>
     <p className="font-sans text-xs font-thin">© <a href="https://github.com/joostmeijles/blog">Joost Meijles</a> 2019</p>
