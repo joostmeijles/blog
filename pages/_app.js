1d7eb87b1c53f218917b417038828f810dce9985
Joost <joost@meijles.com>
2020-02-17 20:46:19 +0100

    Add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Head from 'next/head'
-
+import Router from 'next/router'
+import { initGA, logPageView } from '../lib/analytics'
 import '../tailwind.css'
 import 'typeface-indie-flower'
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    initGA()
+    logPageView()
+    Router.events.on('routeChangeComplete', logPageView)
+  }, [])
+
   return <>
     <Head>
       <meta charSet="utf-8" />
