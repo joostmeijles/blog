8af98344782be91d0999e1a764d21912bcf1d280
Joost <joost@meijles.com>
2020-02-18 20:41:28 +0100

    Fix some more

--- pages/_app.js
+++ pages/_app.js
@@ -17,11 +17,13 @@ export default function MyApp({ Component, pageProps }) {
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
       <meta name="description" content="Software Development by Joost Meijles"/>
+      <meta name="theme-color" content="#FFFFFF"/>
       <meta name="twitter:card" content="summary" key="twitter:card"/>
       <meta name="twitter:site" content="@joostmeijles" key="twitter:site"/>
       <meta name="twitter:creator" content="@joostmeijles" key="twitter:creator"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="apple-touch-icon" href="apple-touch-icon.png"/>
       <link rel="manifest" href="/manifest.json"/>
       <title>Software Development by Joost Meijles</title>
     </Head>
